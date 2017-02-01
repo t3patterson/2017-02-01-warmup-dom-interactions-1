226bb43ad80a707dcf132348e779504e7c7187b1
Travis Hubbard <t3patterson@gmail.com>
2017-02-01 14:18:58 -0500

    cool

--- js/app.js
+++ js/app.js
@@ -1,25 +1,51 @@
+// console.log('wired up!')
+
 //--------------------------------------------------------
 //TASK 1: Add the text 'List of Friends' to .title element  
 //--------------------------------------------------------
 
-
-
+var titleEl = document.querySelector('.title')
+titleEl.textContent += ' List of Friends'
 
 //--------------------------------------------------------
-//TASK 3: Put this array of strings as <li> elements
+//TASK 2: Put this array of strings as <li> elements
 //        onto the friends list 
 //
 //     BONUS: do it in reverse order.
 //--------------------------------------------------------
+var friendsListEl = document.querySelector('.friends-list')
 
-var friendsList = ['Jimmy G.', 'Scotty M.', 'Patty L.', 'Kelly F.', 'Kitty S.']
+var friendsListArr = ['Jimmy G.', 'Scotty M.', 'Patty L.', 'Kelly F.', 'Kitty S.']
+var friendsListArrReversed = friendsListArr.reverse()
 
+var bigHtmlStr = '' 
+
+forEach(friendsListArrReversed , function(arrayElement, index, theArray){
+	bigHtmlStr += "<li>" + arrayElement   + "</li>"
+})
+
+friendsListEl.innerHTML = bigHtmlStr
 
 
 //--------------------------------------------------------
-//TASK 3: Capitalize the text inthe <li> elements with a 
+//TASK 3: Capitalize the text in the <li> elements with a 
 //        class of 'urgent'
 //     
 //        BONUS: Capitalize AND make bold the elements with
 //               a class of 'urgent' 
 //--------------------------------------------------------
+var liTaskEls = document.querySelectorAll('.task-list li.urgent')
+
+forEach( liTaskEls , function(domElement, index, theArray){
+	//PART 1 
+	console.log(domElement)
+	console.log( domElement.textContent.toUpperCase() )
+	var upperCaseTextContent = domElement.textContent.toUpperCase() 
+	domElement.textContent = upperCaseTextContent 
+
+	//BONUS
+	// domElement.innerHTML = '<strong>'  + upperCaseTextContent + '</strong>'
+	
+	console.log('---------')
+}) 
+
---
+++ js/utils.js
@@ -0,0 +1,5 @@
+var forEach = function(arr, func){
+ for(var i = 0 ; i < arr.length; i++){
+  func(arr[i], i, arr)
+ }
+}
