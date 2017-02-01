fff614c0f49af1f12c4c71c2743c8dc63d97ec4a
Travis Hubbard <t3patterson@gmail.com>
2017-02-01 08:57:13 -0500

    ok

---
+++ js/app.js
@@ -0,0 +1,25 @@
+//--------------------------------------------------------
+//TASK 1: Add the text 'List of Friends' to .title element  
+//--------------------------------------------------------
+
+
+
+
+//--------------------------------------------------------
+//TASK 3: Put this array of strings as <li> elements
+//        onto the friends list 
+//
+//     BONUS: do it in reverse order.
+//--------------------------------------------------------
+
+var friendsList = ['Jimmy G.', 'Scotty M.', 'Patty L.', 'Kelly F.', 'Kitty S.']
+
+
+
+//--------------------------------------------------------
+//TASK 3: Capitalize the text inthe <li> elements with a 
+//        class of 'urgent'
+//     
+//        BONUS: Capitalize AND make bold the elements with
+//               a class of 'urgent' 
+//--------------------------------------------------------
